7faffa81fec19bc5f659fab71c060cb898acea9d
Luiscsnz <luiscarlossanchezortega@gmail.com>
2023-05-28 18:36:17 -0500

    Nueva version, agregacion de funcionalidad de edicion

--- Proyecto.py
+++ Proyecto.py
@@ -63,8 +63,33 @@ def eliminar_farmaco():
     # Cerrar conexión
     conn.close()
 
+def editar_farmaco():
+    # Establecer conexión con la base de datos
+    conn = sqlite3.connect('nombres.db')
+
+    # Crear cursor
+    cursor = conn.cursor()
+
+    # Solicitar ID del fármaco a editar
+    id_farmaco = int(input("Ingrese el ID del fármaco a editar: "))
+
+    # Solicitar nuevos datos del fármaco al usuario
+    nuevo_nombre = input("Ingrese el nuevo nombre del fármaco: ")
+    nuevo_precio = float(input("Ingrese el nuevo precio del fármaco: "))
+
+    # Actualizar nombre y precio del fármaco en la tabla
+    cursor.execute("UPDATE farmacos SET nombre = ?, precio = ? WHERE id = ?", (nuevo_nombre, nuevo_precio, id_farmaco))
+
+    # Guardar cambios
+    conn.commit()
+
+    print("Fármaco actualizado correctamente.")
+
+    # Cerrar conexión
+    conn.close()
+
 # Solicitar opción al usuario
-opcion = input("Seleccione una opción:\n1. Mostrar fármacos\n2. Insertar fármaco\n3. Eliminar fármaco\nOpción a seleccionar: \n")
+opcion = input("Seleccione una opción:\n1. Mostrar fármacos\n2. Insertar fármaco\n3. Eliminar fármaco\n4. Editar fármaco\n \nOpción a seleccionar: ")
 
 if opcion == "1":
     mostrar_farmacos()
@@ -72,5 +97,7 @@ elif opcion == "2":
     insertar_farmaco()
 elif opcion == "3":
     eliminar_farmaco()
+elif opcion == "4":
+    editar_farmaco()
 else:
     print("Opción inválida")
